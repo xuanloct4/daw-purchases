5aebca56b9b69c980ef6221a0b5f7082cf01a676
loctv <loc.tran@ascendcorp.com>
2020-10-27 05:52:07 +0700

    add ios xcodeproj

---
+++ ios/daw-purchases/daw-purchases/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  daw-purchases
+//
+//  Created by Tran Loc on 10/27/20.
+//  Copyright © 2020 Tran Loc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios/daw-purchases/daw-purchasesTests/daw_purchasesTests.swift
@@ -0,0 +1,34 @@
+//
+//  daw_purchasesTests.swift
+//  daw-purchasesTests
+//
+//  Created by Tran Loc on 10/27/20.
+//  Copyright © 2020 Tran Loc. All rights reserved.
+//
+
+import XCTest
+@testable import daw_purchases
+
+class daw_purchasesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
